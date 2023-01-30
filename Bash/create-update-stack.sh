5f767afe696aadb576c9d37ea7390f4624e0cd5c
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-01-30 03:34:57 +0200

    Remove RDS

--- Bash/create-update-stack.sh
+++ Bash/create-update-stack.sh
@@ -4,14 +4,14 @@ if aws cloudformation describe-stacks --stack-name $1 --region us-east-1; then
     echo "Stack exists, updating..."
     aws cloudformation update-stack \
         --stack-name $1 \
-        --template-body file://../$2 \
-        --parameters file://../parameters/$3 \
+        --template-body file:/$2 \
+        --parameters file://params/$3 \
         --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
 else
     echo "Stack does not exist, creating..."
     aws cloudformation create-stack \
         --stack-name $1 \
-        --template-body file://../$2 \
-        --parameters file://../parameters/$3 \
+        --template-body file://$2 \
+        --parameters file://params/$3 \
         --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
 fi
